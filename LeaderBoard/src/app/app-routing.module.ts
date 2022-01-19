60d51520777ecdba14121342d39e1e164f31d2a4
priscillamawuana08 <52751969+priscillamawuana08@users.noreply.github.com>
2022-01-19 15:11:15 +0000

    update on the AddUser

--- LeaderBoard/src/app/app-routing.module.ts
+++ LeaderBoard/src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddUserComponent } from './add-user/add-user.component';
 
 const routes: Routes = [
   {path:'', redirectTo:'home',pathMatch:'full'},
+  {path:'add-user',component:AddUserComponent},
+
 ];
 
 @NgModule({
